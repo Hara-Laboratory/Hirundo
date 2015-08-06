f261c60caaedc9353efe0862b36c3e393d9e9089
Tanvir Ahmed <tanvira@cad.ce.titech.ac.jp>
2015-08-06 11:20:29 +0900

    extended subleq is modified

--- src/emulator.c
+++ src/emulator.c
@@ -4,7 +4,7 @@
 
 * Created on : 05-01-2015
 
-* Last Modified on : Mon Aug  3 17:57:30 2015
+* Last Modified on : Thu 06 Aug 2015 11:17:40 AM JST
 
 * Primary Author : Tanvir Ahmed 
 * Email : tanvira@ieee.org
@@ -390,7 +390,8 @@ void exec (uint instruction, uchar *opcode, uchar *funct, uchar *rs, uchar *rt, 
   if (subleq_cond) {
     write_value (subleq_src1, SRC1_LOC);
     write_value (subleq_src2, SRC2_LOC);
-    subleq_machine(routine_addr);
+    //subleq_machine(routine_addr);
+    extended_subleq_machine(routine_addr);
     res_subleq = get_value(DEST_LOC);
   }
 #endif
@@ -720,7 +721,7 @@ void subleq_machine(ushort prog_count) {
 
 
 void extended_subleq_machine (unsigned int prog_count) {
-  while (prog_count == 0) {
+  while (prog_count != 999) {
     unsigned int a = get_value (prog_count);
     unsigned int b = get_value (prog_count + 0x1);
     signed int c = get_value (prog_count + 0x2);
@@ -734,58 +735,60 @@ void extended_subleq_machine (unsigned int prog_count) {
 
     if (subleq) {
       src2 = src2 - src1;
+      write_value (src2, b);
       prog_count = ((src2 > 0) ? prog_count + 0x3 : c);
     }
     else if (extended_subleq) {
-
-
-	src2 = bitReversal (src2);
-        src1 = bitReversal (src1);
-
+      src2 = bitReversal (src2);
+      src1 = bitReversal (src1);
       src2 = src2 - src1;
       prog_count = ((src2 > 0) ? prog_count + 0x3 : (-c));
-        src2 = bitReversal (src2);
+      src2 = bitReversal (src2);
+      write_value (src2, b);
     }
+/*
     else {//xor logic
-    }
+    }*/
   }
 }
 
 
 
 unsigned int bitReversal (unsigned int src) {
-  unsigned int reversedBit = (0xFFFFFFFF << 31) | (src << 31);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 30) & src) << 30);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 29) & src) << 29);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 28) & src) << 28);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 27) & src) << 27);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 26) & src) << 26);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 25) & src) << 25);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 24) & src) << 24);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 23) & src) << 23);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 22) & src) << 22);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 21) & src) << 21);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 20) & src) << 20);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 19) & src) << 19);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 18) & src) << 18);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 17) & src) << 17);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 16) & src) << 16);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 15) & src) << 15);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 14) & src) << 14);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 13) & src) << 13);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 12) & src) << 12);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 11) & src) << 11);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 10) & src) << 10);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 9) & src) << 9);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 8) & src) << 8);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 7) & src) << 7);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 6) & src) << 6);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 5) & src) << 5);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 4) & src) << 4);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 3) & src) << 3);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 2) & src) << 2);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 1) & src) << 1);
-  reversedBit = reversedBit | (((0xFFFFFFFF >> 0) & src) << 0);
+  unsigned int reversedBit = 0x0;
+
+  reversedBit = reversedBit | (((src & 0x1) >> 0) << 31);
+  reversedBit = reversedBit | (((src & 0x2) >> 1) << 30);
+  reversedBit = reversedBit | (((src & 0x4) >> 2) << 29);
+  reversedBit = reversedBit | (((src & 0x8) >> 3) << 28);
+  reversedBit = reversedBit | (((src & 0x10) >> 4) << 27);
+  reversedBit = reversedBit | (((src & 0x20) >> 5) << 26);
+  reversedBit = reversedBit | (((src & 0x40) >> 6) << 25);
+  reversedBit = reversedBit | (((src & 0x80) >> 7) << 24);
+  reversedBit = reversedBit | (((src & 0x100) >> 8) << 23);
+  reversedBit = reversedBit | (((src & 0x200) >> 9) << 22);
+  reversedBit = reversedBit | (((src & 0x400) >> 10) << 21);
+  reversedBit = reversedBit | (((src & 0x800) >> 11) << 20);
+  reversedBit = reversedBit | (((src & 0x1000) >> 12) << 19);
+  reversedBit = reversedBit | (((src & 0x2000) >> 13) << 18);
+  reversedBit = reversedBit | (((src & 0x4000) >> 14) << 17);
+  reversedBit = reversedBit | (((src & 0x8000) >> 15) << 16);
+  reversedBit = reversedBit | (((src & 0x10000) >> 16) << 15);
+  reversedBit = reversedBit | (((src & 0x20000) >> 17) << 14);
+  reversedBit = reversedBit | (((src & 0x40000) >> 18) << 13);
+  reversedBit = reversedBit | (((src & 0x80000) >> 19) << 12);
+  reversedBit = reversedBit | (((src & 0x100000) >> 20) << 11);
+  reversedBit = reversedBit | (((src & 0x200000) >> 21) << 10);
+  reversedBit = reversedBit | (((src & 0x400000) >> 22) << 9);
+  reversedBit = reversedBit | (((src & 0x800000) >> 23) << 8);
+  reversedBit = reversedBit | (((src & 0x1000000) >> 24) << 7);
+  reversedBit = reversedBit | (((src & 0x2000000) >> 25) << 6);
+  reversedBit = reversedBit | (((src & 0x4000000) >> 26) << 5);
+  reversedBit = reversedBit | (((src & 0x8000000) >> 27) << 4);
+  reversedBit = reversedBit | (((src & 0x10000000) >> 28) << 3);
+  reversedBit = reversedBit | (((src & 0x20000000) >> 29) << 2);
+  reversedBit = reversedBit | (((src & 0x40000000) >> 30) << 1);
+  reversedBit = reversedBit | (((src & 0x80000000) >> 31) << 0);
 
   return reversedBit;
 }
